81cbc2461d85dd136a51b307f288e5ee06cdcc04
onuma <a.onuma@1st-scratch.co.jp>
2025-11-27 16:19:07 +0900

    update weather api function to weather_detail.kt view

--- app/src/main/java/com/example/weatherappkotlin/weather_detail.kt
+++ app/src/main/java/com/example/weatherappkotlin/weather_detail.kt
@@ -2,6 +2,7 @@ package com.example.weatherappkotlin
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -24,16 +25,46 @@ class weather_detail : AppCompatActivity() {
          * 選択された地域を表示
          */
         // 1: viewを取得
-        val placeTitle: TextView = findViewById(R.id.tvPlace)
+        val tvPlace: TextView = findViewById(R.id.tvPlace)
+        val tvDetail: TextView = findViewById(R.id.tvDetail)
+        val img_otenki: ImageView = findViewById(R.id.img_otenki)
+        val tvWeather: TextView = findViewById(R.id.tvWeather)
+        val tvTemp: TextView = findViewById(R.id.tvTemp)
+        val tvHumidity: TextView = findViewById(R.id.tvHumidity)
+        val tvWeatherData: TextView = findViewById(R.id.tvWeatherData)
+        val tvTempData: TextView = findViewById(R.id.tvTempData)
+        val tvHumidityData: TextView = findViewById(R.id.tvHumidityData)
         val tvTemperatureDifference: TextView = findViewById(R.id.tvTemperatureDifference)
+        val tvTempMin: TextView = findViewById(R.id.tvTempMin)
+        val tvTempMax: TextView = findViewById(R.id.tvTempMax)
 
         // 2: 渡された値を取り出す
         val placeVal = intent.getStringExtra("SELECTED_PLACE_VALUE")
+
         // JSONオブジェクトを生成
         val jsonObj = JSONObject(placeVal?:"")
-        val cityName = jsonObj.getString("name")
 
-        placeTitle.text = cityName
-        tvTemperatureDifference.text = cityName + "と各地の気温差について"
+        // name
+        val jsonPlace = jsonObj.getString("name")
+
+        // weather
+        val weatherJsonArray = jsonObj.getJSONArray("weather")
+        val weatherJson = weatherJsonArray.getJSONObject(0)
+        val jsonWeather = weatherJson.getString("description")
+
+        // main
+        val mainJson = jsonObj.getJSONObject("main")
+        val jsonTemp = mainJson.getString("temp")
+        val jsonHumidity = mainJson.getString("humidity")
+        val jsonTempMinDiff = mainJson.getString("temp") // 各地との平均が低い場所との気温差（仮）
+        val jsonTempMaxDiff = mainJson.getString("temp") // 各地との平均が高い場所との気温差（仮）
+
+        tvPlace.text = jsonPlace // 地域名
+        tvWeatherData.text = jsonWeather // 天気
+        tvTempData.text = jsonTemp + " 度" //温度
+        tvHumidityData.text = jsonHumidity + " %" // 湿度
+        tvTemperatureDifference.text = jsonPlace + "と各地の気温差について" // 地域気温差タイトル
+        tvTempMin.text = "日本で平均気温が低い「陸別（現在${jsonTempMinDiff}度）」より${jsonTempMinDiff}度暖かいです" // 各地との平均が低い場所との気温差（仮）
+        tvTempMax.text = "日本で平均気温が高い「沖縄（現在${jsonTempMaxDiff}度）」より${jsonTempMaxDiff}度暖かいです" // 各地との平均が高い場所との気温差（仮）
     }
 }
